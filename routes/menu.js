84d970c1d3ee97856f00d6c42e51677df4dc4651
VTAIT <vta.laptrinh@gmail.com>
2023-11-23 13:27:09 +0700

    update menu

--- routes/menu.js
+++ routes/menu.js
@@ -5,9 +5,9 @@ const controller = require("../controllers/MenuController");
 const asyncHandler = require("express-async-handler");
 const authMdw = require("../middlewares/authMdw");
 
-router.use(authMdw);
-router.use("/create", asyncHandler(controller.create));
-router.use("/update", asyncHandler(controller.update));
+// router.use(authMdw);
+router.use("/create", authMdw, asyncHandler(controller.create));
+router.use("/update", authMdw, asyncHandler(controller.update));
 router.use("/getById", asyncHandler(controller.getById));
 router.use("/", asyncHandler(controller.index));
 
